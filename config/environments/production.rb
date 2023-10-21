596ebe6485bccd6afea029a6b2c8aed03cf2b4ff
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-10-21 11:55:10 +0900

    🐞fix：add configratin in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Custom configuration
+  config.action_dispatch.tld_length = 2
 end
