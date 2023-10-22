f06eed0161daf38e4d20a5e1b70a4d978f0cf88d
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-10-23 08:28:05 +0900

    🐞fix：update directory structure #16

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-bundle install
-
-if [ ! -e "/backend/config/routes.rb" ]; then
-  echo 'rails new APIモード を実行する'
-  # --skip入れないとpgのgemないってエラーが出る
-  rails new . --force --api --database=postgresql --skip-git --skip-bundle
-  bundle install
-fi
-
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /backend/tmp/pids/server.pid
 
