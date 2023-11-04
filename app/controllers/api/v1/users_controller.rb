4f9fb796924f304827367cb532668fcd8c1f3333
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-11-04 15:58:33 +0900

    ✨feat：add show action for userscontroller #25

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+  def show
+    user = User.find_by!(uid: params[:uid])
+    if user
+      render json: user, status: :ok
+    else
+      render json: { error: 'ユーザーが見つかりません' }, status: :not_found
+    end
+  end
+
   def create
     # 条件に該当するデータがあればそれを返す。なければ新規作成
     user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
