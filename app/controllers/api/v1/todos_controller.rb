f5c23537b9cee7644ad5ddf0700b1ae9f73f8948
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-11-05 23:18:01 +0900

    🐞fix：fix todos and users controller #29

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,6 +1,29 @@
 class Api::V1::TodosController < ApplicationController
   before_action :set_todo, only: %i[show update destroy]
 
+  # def todos_by_user
+  #   user_id = params[:user_id]
+  #   @todos = Todo.where(user_id: user_id)
+
+  #   if @todos.any?
+  #     render json: @todos
+  #   else
+  #     render json: { error: "指定されたユーザーのToDoが見つかりません" }, status: :not_found
+  #   end
+  # end
+
+  def todos_by_uid
+    uid = params[:uid]
+    user = User.find_by(uid: uid)
+
+    if user.present?
+      @todos = Todo.where(user_id: user.id)
+      render json: @todos
+    else
+      render json: { error: "指定されたUIDのユーザーが見つかりません" }, status: :not_found
+    end
+  end
+
   def index
     @todos = Todo.all
     render json: @todos
@@ -27,24 +50,55 @@ class Api::V1::TodosController < ApplicationController
   #   end
   # end
 
+  # def create
+  #   @todo = Todo.new(todo_params)
+  #   categories = params[:category_ids]&.split(" ") # category_idsが存在しない場合にnilを返す
+
+  #   if @todo.save
+  #     if categories.present?
+  #       categories.each do |category|
+  #         TodoCategory.create(todo_id: @todo.id, category_id: category)
+  #       end
+  #     end
+
+  #     category_list = {
+  #       todo_id: @todo.id,
+  #       categories: categories || [] # categoriesが存在しない場合は空の配列を返す
+  #     }
+  #     render json: category_list
+  #   else
+  #     render json: { error: "保存に失敗しました" }, status: :unprocessable_entity
+  #   end
+  # end
+
   def create
-    @todo = Todo.new(todo_params)
-    categories = params[:category_ids]&.split(" ") # category_idsが存在しない場合にnilを返す
+    # Find the user by uid from the request body
+    uid = params[:uid]
+    user = User.find_by(uid: uid)
 
-    if @todo.save
-      if categories.present?
-        categories.each do |category|
-          TodoCategory.create(todo_id: @todo.id, category_id: category)
+    if user.present?
+      # Create a new todo and associate it with the user
+      @todo = Todo.new(todo_params)
+      @todo.user = user # Associate the todo with the user
+      categories = params[:category_ids]&.split(" ")
+
+      if @todo.save
+        if categories.present?
+          categories.each do |category|
+            TodoCategory.create(todo_id: @todo.id, category_id: category)
+          end
         end
-      end
 
-      category_list = {
-        todo_id: @todo.id,
-        categories: categories || [] # categoriesが存在しない場合は空の配列を返す
-      }
-      render json: category_list
+        category_list = {
+          todo_id: @todo.id,
+          categories: categories || [] # categories that are nil will be an empty array
+        }
+        render json: category_list
+      else
+        render json: { error: "保存に失敗しました" }, status: :unprocessable_entity
+      end
     else
-      render json: { error: "保存に失敗しました" }, status: :unprocessable_entity
+      render json: { error: "指定されたUIDのユーザーが見つかりません" }, status: :not_found
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # resources :users, only: [:index]
-      resources :users, only: :show, param: :uid
-      resources :todos, only: [:index, :create, :show, :destroy, :update]
+      # resources :users, only: :show, param: :uid
+      resources :todos, only: [:index, :create, :show, :destroy, :update] do
+        collection do
+          get 'todos_by_uid/:uid', action: 'todos_by_uid'
+        end
+      end
       resources :categories, only: [:index, :create, :destroy, :update]
       resources :hello, only: [:index]
     end
