6bbb8f30e2c3de6f2044f5b72d4579c5e9a5072c
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-11-22 10:29:06 +0900

    👷chore：add log msg for users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    puts "ログ！！Received request with params: #{params.inspect}"
     user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
     if user
       head :ok
@@ -7,6 +8,7 @@ class Api::V1::UsersController < ApplicationController
       render json: { error: "ログインに失敗しました" }, status: :unprocessable_entity
     end
   rescue StandardError => e
+    puts "ログ！！Error in UsersController#create: #{e.message}"
     render json: { error: e.message }, status: :internal_server_error
   end
 end
