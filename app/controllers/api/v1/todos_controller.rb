52e60e5aabd786e6fae77ac7467246e5fdafb01d
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-03-04 21:40:02 +0900

    🎨 新規ToDo時のカテゴリー追加ロジック修正 #81

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -50,13 +50,16 @@ class Api::V1::TodosController < ApplicationController
 
   def create
     @todo = @user.todos.build(todo_params)
-    category_ids = params[:category_ids] || []
 
     if @todo.save
+      categories_params = params["categories"] || []
+      category_ids = categories_params.map { |category| category["id"] }
+      # 新しいカテゴリーを追加
       category_ids.each do |category_id|
-        TodoCategory.create(todo_id: @todo.id, category_id: category_id)
+        category = Category.find_by(id: category_id)
+        @todo.categories << category if category.present?
       end
-      render_todo_with_categories(@todo, category_ids)
+      render json: @todo
       AchievementCalculator.calculate_and_update_achievements(@user, @todo)
     else
       render_error("保存に失敗しました", :unprocessable_entity)
