098cbcb114e613734b98fdfb12945ce7cd172a53
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-03-23 18:25:53 +0900

    ✅ 成功ケースの検証 #88

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :todo do
+    user_id { 1 }
     title { "MyString" }
+    description { "MyText" }
+    due_date { Date.today + 7 } # Due date set to 7 days from today
+    completed { false }
+    zone { 0 }
+    association :user, factory: :user # Assuming you have a user factory defined
   end
 end
--- spec/requests/todos_apis_spec.rb
+++ spec/requests/todos_apis_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 describe 'GET todos_by_uid/:uid' do
   it '指定したユーザーのToDo取得' do
-    FactoryBot.create_list(:todo, 10)
-    get 'todos_by_uid/:uid'
+    user = create(:user)
+    todo = create(:todo, user_id: user.id)
+
+    get "/api/v1/todos/todos_by_uid/#{user.uid}"
     json = JSON.parse(response.body)
-    expect(response.status).to eq(200)
-    expect(json.length).to eq(1)
+    expect(response).to have_http_status(:success)
+    # expect(json['todos'].length).to eq(1) # 修正箇所
+    # expect(json['todos'][0]['title']).to eq(todo.title)
   end
 end
